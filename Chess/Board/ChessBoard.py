a6f42992459a2c935c8ea73b75ecc3af1e1dc839
CameliaMallus <cameliamallus@gmail.com>
2022-08-07 21:54:11 +0200

    π Func chess_board(FEN_string) Finished Prints out in __main__

--- Chess/Board/ChessBoard.py
+++ Chess/Board/ChessBoard.py
@@ -1,2 +1,29 @@
-def Chessboard(position_string):
-    pass
+def chess_board(FEN_string):
+    """Makes the FEN string given a list of 8 lists with each a length of 8, representing the chessboard when printed out"""
+    FEN_list = FEN_string.split(" ")
+    position_list = [[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0]]
+    position_listx = 0
+    position_listy = 0
+    numbers = ["1","2","3","4","5","6","7","8"]
+    for x in FEN_list[0]:
+        if (x) == "/":
+            position_listx=0
+            position_listy+=1
+        elif any(x==n for n in numbers):
+            x=int(x)
+            position_listx+=x
+        else:
+            position_list[position_listy][position_listx] = x
+            position_listx+=1
+    if __name__=="__main__":
+        print("\n")
+        for y in range(8):
+            for x in range(8):
+                print(position_list[y][x], end="   ")
+            print("\n")
+        print("\n")
+        
+        
+
+
+chess_board("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
