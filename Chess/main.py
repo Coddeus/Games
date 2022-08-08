c6f51861d435f782eb4f5049b4b4a4b104243d3f
CameliaMallus <cameliamallus@gmail.com>
2022-08-08 16:07:08 +0200

    🧹 Cleaned func draw_board Earned style points on the window Added .gitignore

--- Chess/main.py
+++ Chess/main.py
@@ -1,5 +1,6 @@
 from Board import chess_board
 import pygame as d
+import os
 
 def count_combinations(n):
     """Counts the number of possible move combinations from the beginning of the game after n total moves"""
@@ -10,22 +11,27 @@ def count_positions(n):
 def draw_board(FEN_string):
     list = chess_board(FEN_string)
     
-    d.init()
-    window = d.display.set_mode((800, 800), d.NOFRAME)
+    running = True
+    window = d.display.set_mode((800, 800))
+    icon = d.image.load("Chess\Assets\Icons\WindowIcon.png")
     white = d.Color(255,255,255)
     black = d.Color(0,0,0)
+    d.init()
     window.fill(white)
+    d.display.set_icon(icon)
+    d.display.set_caption('Chess')
     for y in range(8):
         for x in range(8):
             if (x+y)%2==1:
                 d.draw.rect(window, black, (x*100, y*100, 100, 100))
+    d.display.update()
 
-    while True:
+    while running:
         for event in d.event.get():
-            if event.type == d.KEYDOWN and event.key == d.K_ESCAPE: 
-                d.quit()
-        d.display.update()
-
+            if (event.type == d.KEYDOWN and event.key == d.K_ESCAPE) or (event.type == d.QUIT): 
+                running = False
+        
+    
     
 
 draw_board("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
