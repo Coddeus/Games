a764a006e89d526c0307882208ea95eab2f22227
CameliaMallus <cameliamallus@gmail.com>
2022-08-10 11:07:09 +0200

    🧹 looped images loading

--- Chess/main.py
+++ Chess/main.py
@@ -12,26 +12,14 @@ def count_positions(n):
 
 def draw_board(FEN_string):
     list = chess_board(FEN_string)
-    
-    running = True
     #let user customize below info in settings
     window = d.display.set_mode((920, 890), d.SCALED)
     icon = d.image.load("Assets\Icons\WindowIcon.png")
     white = d.Color(255,255,255)
     light = d.Color(172, 115, 57)
     dark = d.Color(102, 51, 0)
-    p = d.image.load(os.path.join('Assets', 'Pieces','bp.png')).convert_alpha()
-    P = d.image.load(os.path.join('Assets', 'Pieces','wP.png')).convert_alpha()
-    n = d.image.load(os.path.join('Assets', 'Pieces','bn.png')).convert_alpha()
-    N = d.image.load(os.path.join('Assets', 'Pieces','wN.png')).convert_alpha()
-    b = d.image.load(os.path.join('Assets', 'Pieces','bb.png')).convert_alpha()
-    B = d.image.load(os.path.join('Assets', 'Pieces','wB.png')).convert_alpha()
-    r = d.image.load(os.path.join('Assets', 'Pieces','br.png')).convert_alpha()
-    R = d.image.load(os.path.join('Assets', 'Pieces','wR.png')).convert_alpha()
-    q = d.image.load(os.path.join('Assets', 'Pieces','bq.png')).convert_alpha()
-    Q = d.image.load(os.path.join('Assets', 'Pieces','wQ.png')).convert_alpha()
-    k = d.image.load(os.path.join('Assets', 'Pieces','bk.png')).convert_alpha()
-    K = d.image.load(os.path.join('Assets', 'Pieces','wK.png')).convert_alpha()
+    for x in ["bp", "wP", "bn", "wN", "bb", "wB", "br", "wR", "bq", "wQ", "bk", "wK"]:
+        locals()[x[1]] = d.image.load(os.path.join('Assets', 'Pieces', x+'.png')).convert_alpha()
     pxy = Pxy = (23,18)
     nxy = Nxy = (21,18)
     bxy = Bxy = (18,18)
@@ -55,9 +43,10 @@ def draw_board(FEN_string):
             else:
                 window.blit(locals()[list[y][x]], (100*x+60+locals()[list[y][x]+"xy"][0],100*y+30+locals()[list[y][x]+"xy"][0]))
     d.display.update()
-
+    running = True
     while running:
         for event in d.event.get():
+            print(event)
             if (event.type == d.KEYDOWN and event.key == d.K_ESCAPE) or (event.type == d.QUIT): 
                 running = False
         
