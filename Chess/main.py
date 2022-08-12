a8468944397e5691fcce98351650dd3e49f95144
CameliaMallus <cameliamallus@gmail.com>
2022-08-13 01:48:19 +0200

    Limited framerate to 100fps

--- Chess/main.py
+++ Chess/main.py
@@ -4,16 +4,19 @@ from math import floor
 # TODO make a menu bar
 # TODO add different resolutions (1 big and others are smallered img resolutions ?)
 # Global variables declaring
+clock = d.time.Clock()
 window = d.display.set_mode((800, 800), d.SCALED)
 icon = d.image.load("Assets\Icons\WindowIcon.png")
 white = d.Color(255,255,255)
-light = d.Color(172, 115, 57) # TODO let user customize squares colors
+light = d.Color(172, 115, 57) # TODO let user customize squares colors (and what else ?)
 dark = d.Color(102, 51, 0)
 lightblue = d.Color(100, 100, 255)
 darkblue = d.Color(40, 40, 100)
 for x in ["bp", "wP", "bn", "wN", "bb", "wB", "br", "wR", "bq", "wQ", "bk", "wK"]:
 	globals()[x[1]] = d.image.load(path.join('Assets', 'Pieces', x+'.png')).convert_alpha()
 possible_pieces = [["P","N","B","R","Q","K"], ["p","n","b","r","q","k"]]
+startsquarex = 0 # TODO color the start/end squares of the letest move
+startsquarey = 0
 pxy = Pxy = (23,18)
 nxy = Nxy = (21,18)
 bxy = Bxy = kxy = Kxy = (18,18)
@@ -27,7 +30,7 @@ def list_to_str(chess_board):
 def str_to_list(FEN_string): # TODO str -> list
 	"""Turns the FEN string given into a list of 8 lists with each a length of 8, representing the chessboard"""
 	FEN_list = FEN_string.split(" ")
-	position_list = [[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,1,1,0,0,1],[[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0]]]
+	position_list = [[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,1,1,0,0,1]]
 	position_listx = 0
 	position_listy = 0
 	numbers = ["1","2","3","4","5","6","7","8"]
@@ -62,6 +65,7 @@ def blit_on_cursor(piece):
 	window.blit(globals()[piece], coordinates)
 
 def init(FEN_string):
+	"""Initializes with a FEN string or a custom format list from this file""" # TODO other formats to read ?
 	if "/" in FEN_string:
 		list = str_to_list(FEN_string)
 
@@ -77,21 +81,14 @@ def init(FEN_string):
 	running = True
 	dragged = False
 	while running: # TODO add chess rules
-		for event in d.event.get(): #TODO review ifs order, makes it user-friendly with several clicks at a time
+		for event in d.event.get(): #TODO review ifs order, makes it user-friendly with several clicks at a time (customizable)
+
 			squarey=floor(d.mouse.get_pos()[1]/100)
 			squarex=floor(d.mouse.get_pos()[0]/100)
 
 			if (event.type == d.KEYDOWN and event.key == d.K_ESCAPE) or (event.type == d.QUIT): 
 				running = False
 			
-			elif d.mouse.get_pressed(5)[2]==True: #TODO add other colors with right click and alt / ctrl 
-				if (squarex+squarey)%2==1: #TODO add mode with mousebuttondown to color only one square
-					d.draw.rect(window, darkblue, (squarex*100, squarey*100, 100, 100))
-				else:
-					d.draw.rect(window, lightblue, (squarex*100, squarey*100, 100, 100))
-				if list[squarey][squarex]!=0:
-					window.blit(globals()[list[squarey][squarex]], (100*squarex+globals()[list[squarey][squarex]+"xy"][0],100*squarey+globals()[list[squarey][squarex]+"xy"][1]))
-
 			elif buttons[0]==False and d.mouse.get_pressed(5)[0]==True:
 				draw_board(list)
 				if list[squarey][squarex] in possible_pieces[list[8][0]%2]:
@@ -112,9 +109,20 @@ def init(FEN_string):
 				window.blit(globals()[piece], end_coordinates)
 				list[8][0]+=1
 				dragged = False
+			
+			elif d.mouse.get_pressed(5)[2]==True: #TODO add other colors with right click and alt / ctrl 
+				if (squarex+squarey)%2==1: #TODO add mode with mousebuttondown to color only one square / choose to draw multipleSquares or draw arrow
+					d.draw.rect(window, darkblue, (squarex*100, squarey*100, 100, 100))
+				else:
+					d.draw.rect(window, lightblue, (squarex*100, squarey*100, 100, 100))
+				if list[squarey][squarex]!=0:
+					window.blit(globals()[list[squarey][squarex]], (100*squarex+globals()[list[squarey][squarex]+"xy"][0],100*squarey+globals()[list[squarey][squarex]+"xy"][1]))
+
+			
 
 			buttons = d.mouse.get_pressed(5)
 			d.display.update()
+			clock.tick(100)
 	d.quit()
 
 def count_combinations(n):
